7cb6c07eb48d88193187c6db92bf0fbc085ed00d
Your Name <tomposki@gmail.com>
2021-03-30 21:32:40 +0200

    Save to Excel file

--- __main__.py
+++ __main__.py
@@ -1,4 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import openpyxl
 import pandas as pd
 
-sales = pd.read_csv(r'.\data\sales_data.csv')['Customer_Age'].mean()
-print(sales)
+
+sales = pd.read_csv(r'.\data\sales_data.csv', parse_dates=['Date'])
+# sales = pd.read_csv(r'.\data\sales_data.csv')
+# df = pd.DataFrame(sales)
+print(sales.describe())
+# sales['Customer_Age'].mean()
+df = pd.DataFrame(sales.describe())
+df.to_excel('test.xlsx')
