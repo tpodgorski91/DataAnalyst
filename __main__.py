851e37610fc595798cf384e2af30b042d8dc8624
Your Name <tomposki@gmail.com>
2021-03-31 17:00:30 +0200

    numpy arange method

--- __main__.py
+++ __main__.py
@@ -3,11 +3,23 @@ import numpy as np
 import openpyxl
 import pandas as pd
 
+file = r'.\data\sales_data.csv'
+# sales = pd.read_csv(r'.\data\sales_data.csv', parse_dates=['Date'])
 
-sales = pd.read_csv(r'.\data\sales_data.csv', parse_dates=['Date'])
-# sales = pd.read_csv(r'.\data\sales_data.csv')
-# df = pd.DataFrame(sales)
-print(sales.describe())
+# print(sales.describe())
 # sales['Customer_Age'].mean()
-df = pd.DataFrame(sales.describe())
-df.to_excel('test.xlsx')
+# df = pd.DataFrame(sales.describe())
+# df.to_excel('test.xlsx')
+
+# E: Create a numpy array with values ranging from 10 to 49
+
+# my solution
+# a = np.array([10])
+# b = []
+# for i in range(11, 50):
+#     b.append(i)
+# newArray = np.append(a, b)
+# print(newArray)
+
+# easier solution
+# print(np.arange(10, 50))
