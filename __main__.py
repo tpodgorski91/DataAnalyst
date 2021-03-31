22e1c6fbe764dae1d107582d0df329d4afcf3ff5
Your Name <tomposki@gmail.com>
2021-03-31 20:12:23 +0200

    explore more numpy

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import openpyxl
 import pandas as pd
+import sys
 
 file = r'.\data\sales_data.csv'
 # sales = pd.read_csv(r'.\data\sales_data.csv', parse_dates=['Date'])
@@ -23,3 +24,43 @@ file = r'.\data\sales_data.csv'
 
 # easier solution
 # print(np.arange(10, 50))
+
+# Create a numpy matrix of 2*2 integers, filled with ones
+# print(np.ones([2,3], dtype=int))
+
+# print(np.ones([3, 2], dtype=np.float16))
+# print(np.zeros([3, 2], dtype=np.float16))
+
+# Create a numpy matrix of 4*4 integers, filled with fives.
+# print(
+#
+#     np.array([5]*16, dtype=int).reshape(4, 4)
+# )
+
+# a = np.array([10, 2, 100])
+# print(np.ones_like(a) * 5)
+
+# convert numpy array from the list
+# copy array
+# append to the array
+
+# l = [1, 2, 3, 4]
+#
+# a = np.array(l)
+# y = np.copy(a)
+# y = np.append(y, [50])
+#
+# print(
+#     a,
+#     y
+# )
+# Comparing size of list
+
+a = np.random.randint(0,8,9,)
+l = [1,2,3,4,5,6,7,8]
+print(
+a,
+sys.getsizeof(1),
+sys.getsizeof(int),
+np.dtype(int).itemsize
+)
